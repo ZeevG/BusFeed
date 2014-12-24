f03b070685dd7b82738c0d2cad4fed632caac4c1
Ze'ev Gilovitz <zeev.gil@gmail.com>
2014-12-24 23:07:04 +0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name busFeedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the busFeedApp
+ */
+angular.module('busFeedApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
