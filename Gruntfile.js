55102b6f6d419815f122b7327476d65e09abb3db
Ze'ev Gilovitz <zeev.gil@gmail.com>
2014-12-29 11:21:49 +0800

    fixed problem with font-awesome build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -353,6 +353,13 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+        //for font-awesome
+            expand: true,
+            dot: true,
+            cwd: 'bower_components/fontawesome',
+            src: ['fonts/*.*'],
+            dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
