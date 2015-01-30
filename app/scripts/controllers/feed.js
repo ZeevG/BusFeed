1b9609c858ef4f9ead5351ee04ed3e09a38326fb
Ze'ev Gilovitz <zeev.gil@gmail.com>
2015-01-30 15:23:07 +1100

    Added a map with markers and travel path

--- app/scripts/controllers/feed.js
+++ app/scripts/controllers/feed.js
@@ -15,6 +15,11 @@ angular.module('busFeedApp')
 
     $scope.routes = [];
 
+    $scope.toggleDetails = function(index){
+      $scope.routes[index].detailsVisible = !$scope.routes[index].detailsVisible;
+      console.log(index);
+    };
+
     // Time interval in miliseconds between polls
     var refreshInterval = 120000;
 
@@ -39,7 +44,29 @@ angular.module('busFeedApp')
 
         console.log(DirectionsStatus);
         for(var ii=0;ii<directions.routes.length;ii++){
+          var bounds = directions.routes[ii].bounds;
+          var leg = directions.routes[ii].legs[0]; // Not using waypoints so always only one leg
+          
           directions.routes[ii].md5 = MD5.hex(angular.toJson(directions.routes[ii]));
+          directions.routes[ii].detailsVisible = false;
+
+          directions.routes[ii].center = {
+            'latitude': bounds.getCenter().lat(),
+            'longitude': bounds.getCenter().lng()
+          };
+          directions.routes[ii]
+          directions.routes[ii].markers = [
+            {
+              'id': (ii*10)+1,
+              'latitude': leg.start_location.lat(),
+              'longitude': leg.start_location.lng()
+            },
+            {
+              'id': (ii*10)+2,
+              'latitude': leg.end_location.lat(),
+              'longitude': leg.end_location.lng()
+            }
+          ];
         }
 
         $scope.$apply(function(){
