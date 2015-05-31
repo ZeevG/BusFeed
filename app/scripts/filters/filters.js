74e50d495c8ac3324ab23a8ad2810d37a1453ca2
Ze'ev Gilovitz <zeev.gil@gmail.com>
2015-05-31 15:00:00 +0800

    Transit icons now work for trains

--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -12,16 +12,28 @@ var iconSeperator = '<i class="fa fa-angle-right fa-'+sizeMultiplier+'x"></i>';
 angular.module('busFeedApp.filters', [])
   .filter('iconise_steps', function(){
     return function(steps){
+
+      // Whoa, this is a little confusing...
+      // Map each step to an icon
       var icons = steps.map(function(step){
         var route = '';
 
         if(step.travel_mode === 'TRANSIT'){
-          route = step.transit.line.short_name;
+          if(step.transit.line.vehicle.type === 'BUS'){
+            route = step.transit.line.short_name;
+          }else{
+            route = step.transit.line.name;
+          }
+
+
+          return '<i class="fa fa-2x"><img src="'+step.transit.line.vehicle.icon+'">&nbsp;'+route+'</i>';
+
         }
 
         return '<i class="'+iconClassName[step.travel_mode]+'">&nbsp;'+route+'</i>';
       });
 
+      // Join each icon with a seperator
       return '<span class="steps-container">'+
               icons.join(iconSeperator)+
               '</span>';
